9376e072f3e1a1e80148b919faceab4a918f7769
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-04-25 11:17:45 +0800

    Create build.sh

---
+++ .circleci/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo mkdir /build
+sudo chmod +666 /build
+GO111MODULE=on go get -d ./...
+
+go_build(){
+GO111MODULE=on GOOS=$1 GOARCH=$2 go build -o /build/$1_$2 $3
+}
+
+go_build "linux" "amd64" $1
+go_build "linux" "386" $1
+go_build "linux" "arm" $1
+go_build "linux" "arm64" $1
+go_build "windows" "amd64" $1
+go_build "windows" "386" $1
+go_build "darwin" "amd64" $1
+go_build "darwin" "386" $1
+
+for i in /build/windows*;do mv $i $i.exe; done
+
